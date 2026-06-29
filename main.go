a6e3a74d8f9a04f078a2ee029d7e96627e5cc909
Jack Aldus <jack.aldus@gmail.com>
2026-06-29 21:31:25 +0100

    B: repl CLI basic functionality implemented in main.go using bufio package

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+		if !ok {
+			if err := scanner.Err(); err != nil {
+				return
+			}
+			return
+		}
+
+		text := scanner.Text()
+
+		words := cleanInput(text)
+		if len(words) == 0 {
+			continue
+		}
+
+		cliInput := words[0]
+		fmt.Printf("Your command was: %s\n", cliInput)
+	}
 }
